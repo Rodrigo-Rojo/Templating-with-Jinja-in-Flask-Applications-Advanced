3f0145c16f559d39b9399143403197438ff4df3a
Rodrigo Rojo <82297469+Rodrigo-Rojo@users.noreply.github.com>
2021-10-16 12:06:18 -0600

    Add files via upload
    Contact form wasn't functional now it works getting the user inputs and sending an email to the admin.

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import datetime
 from data import data
+import smtplib
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+EMAIL = os.environ.get("EMAIL")
+PASSWORD = os.environ.get("PASSWORD")
 today = datetime.datetime.today()
 app = Flask(__name__)
+
+def send_email(name, email, number, message):
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs=email,
+            msg=f"Subject: SoriOner's Blog Website Contact\n\n"
+                f"{name} want to hear from you\n"
+                f"{name} Phone Number: {number}\n"
+                f"message: {message}"
+        )
+
 
 @app.route('/')
 def home():
@@ -27,6 +50,20 @@ def post(id):
 
 def footer():
     return render_template("footer.html", date=today)
+
+
+@app.route('/message', methods=['POST'])
+def message():
+    name = request.form['name']
+    email = request.form['email']
+    phone = request.form['phone']
+    message = request.form['message']
+    try:
+        send_email(name, email, phone, message)
+    except Exception as e:
+        print(e)
+
+    return render_template("true.html", date=today)
 
 
 if __name__ == "__main__":
